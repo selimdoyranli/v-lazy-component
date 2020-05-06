16ed488a8893d95d090b4019c010ec43205ab1b4
selimdoyranli <selim@radkod.com>
2020-05-06 22:53:13 +0300

    :tada: init

---
+++ src/v-lazy-component.vue
@@ -0,0 +1,84 @@
+<template>
+  <component
+    :is="wrapperTag"
+    :class="['v-lazy-component', {'loading': !isIntersected, 'loaded': isIntersected}]"
+    :style="{
+      minWidth: '1px',
+      minHeight: '1px',
+    }"
+  >
+    <slot v-if="isIntersected" />
+    <!-- Content that is loaded as a placeholder until it comes into view -->
+    <slot v-if="!isIntersected" name="placeholder" />
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    wrapperTag: {
+      type: String,
+      required: false,
+      default: "div"
+    },
+    /**
+     * See IntersectionOberserver rootMargin [docs](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options)
+     */
+    rootMargin: {
+      type: String,
+      required: false,
+      default: "0px 0px 0px 0px"
+    },
+    /**
+     * See IntersectionOberserver treshold [docs](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options)
+     */
+    threshold: {
+      type: [Number, Array],
+      required: false,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      isIntersected: false,
+      observer: null
+    };
+  },
+  watch: {
+    isIntersected(value) {
+      if (value) {
+        this.$emit("intersected", this.$el);
+      }
+    }
+  },
+  mounted() {
+    if ("IntersectionObserver" in window) {
+      this.observe();
+    } else {
+      this.isIntersected = true;
+    }
+  },
+  beforeDestroy() {
+    this.unobserve();
+  },
+  methods: {
+    observe() {
+      const { rootMargin, threshold } = this;
+      const config = { root: undefined, rootMargin, threshold };
+      this.observer = new IntersectionObserver(this.onIntersection, config);
+      this.observer.observe(this.$el);
+    },
+    onIntersection(entries) {
+      this.isIntersected = entries.some(entry => entry.intersectionRatio > 0);
+      if (this.isIntersected) {
+        this.unobserve();
+      }
+    },
+    unobserve() {
+      if ("IntersectionObserver" in window) {
+        this.observer.unobserve(this.$el);
+      }
+    }
+  }
+};
+</script>
